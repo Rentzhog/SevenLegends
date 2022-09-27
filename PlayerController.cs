1ea7b0a7703c4ccc47998fa3041d4b4a55b24822
Rentzhog <55357873+Rentzhog@users.noreply.github.com>
2022-09-27 16:13:50 +0200

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     Rigidbody2D rb;
     CapsuleCollider2D playerCollider;
 
@@ -23,31 +22,38 @@ public class PlayerController : MonoBehaviour
     public float moveSmoothTime = 0.3f;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
+    public float inAirSmoothing = .5f;
+    public float fallMultiplierBuffer = 1f;
 
     [Header("Ground")]
     public LayerMask groundLayer;
 
     bool fastFalling;
 
+    bool canDash = true;
+    bool isDashing;
+
     bool grounded;
     float groundedRaycastLength = .3f;
 
-    private float currentDir;
-    private float currentDirVeloctiy;
+    float targetInput;
+    private float currentInput;
+    private float currentInputVelocity;
+
+    private float currentSpeed;
+    private float currentSpeedChange;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         currentJumpsLeft = maxJumps;
         rb = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        float targetDir = Input.GetAxisRaw("Horizontal");
-        currentDir = Mathf.SmoothDamp(currentDir, targetDir, ref currentDirVeloctiy, moveSmoothTime);
+    void Update() {
+        targetInput = Input.GetAxisRaw("Horizontal");
+        currentInput = Mathf.SmoothDamp(currentInput, targetInput, ref currentInputVelocity, moveSmoothTime);
 
         if (Input.GetButtonDown("Jump") && currentJumpsLeft > 0) {
             Jump();
@@ -59,17 +65,26 @@ public class PlayerController : MonoBehaviour
         else {
             fastFalling = false;
         }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash) {
+            StartCoroutine(Dash());
+        }
     }
 
     private void FixedUpdate() {
+
+        if (isDashing) {
+            return;
+        }
 
         GroundCheck();
 
         // Better gravity
 
-        if (rb.velocity.y < 0) {
+        if (rb.velocity.y < fallMultiplierBuffer) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
-        }else if(rb.velocity.y > 0 && !Input.GetButton("Jump")) {
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
         }
 
@@ -80,8 +95,17 @@ public class PlayerController : MonoBehaviour
             rb.gravityScale = gravityScale;
         }
 
+        float targetVelocity = currentInput * speed * Time.fixedDeltaTime;
+        print(rb.velocity.y);
         // Set player velocity
-        rb.velocity = new Vector2(currentDir * speed * Time.fixedDeltaTime, rb.velocity.y);
+        if (grounded) {
+            currentSpeed = targetVelocity;
+        }
+        else {
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, targetVelocity, ref currentSpeedChange, inAirSmoothing);
+        }
+
+        rb.velocity = new Vector2(currentSpeed, rb.velocity.y);
     }
 
     void GroundCheck() {
@@ -103,17 +127,36 @@ public class PlayerController : MonoBehaviour
 
         Debug.DrawLine(new Vector2(playerCollider.bounds.min.x + margin, transform.position.y), new Vector2(playerCollider.bounds.min.x + margin, transform.position.y - groundedRaycastLength), Color.red);
         Debug.DrawLine(new Vector2(playerCollider.bounds.max.x - margin, transform.position.y), new Vector2(playerCollider.bounds.max.x - margin, transform.position.y - groundedRaycastLength), Color.red);
- 
+
     }
 
     void Jump() {
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         currentJumpsLeft--;
+        if (isDashing) {
+            print("BANG");
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.otherCollider.gameObject.layer == LayerMask.GetMask(groundLayer)) {
-            rb.velocity = new Vector2(0, rb.velocity.y);
+    IEnumerator Dash() {
+        canDash = false;
+        isDashing = true;
+        float gravScale = rb.gravityScale;
+        rb.gravityScale = 0;
+
+        if(targetInput < 0) {
+            rb.velocity = new Vector2(-dashForce, 0);
+            currentSpeed = -dashForce;
         }
+        else if(targetInput > 0) {
+            rb.velocity = new Vector2(dashForce, 0);
+            currentSpeed = dashForce;
+        }
+
+        yield return new WaitForSeconds(dashTime);
+        rb.gravityScale = gravScale;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
