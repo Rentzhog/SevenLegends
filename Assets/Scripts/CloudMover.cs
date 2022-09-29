daeba12daff2ef984e8833cf817e6e912ef24cf2
Rentzhog <55357873+Rentzhog@users.noreply.github.com>
2022-09-30 01:12:40 +0200

    Add files via upload

---
+++ Assets/Scripts/CloudMover.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMover : MonoBehaviour
+{
+
+    public GameObject[] cloudPrefabs;
+
+    public int clouds;
+    public float speed;
+
+    private void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Transform child in transform.GetComponentsInChildren<Transform>()) {
+            child.Translate(Vector2.right * Time.deltaTime * speed * Random.Range(0.8f, 1f));
+        }
+    }
+}
